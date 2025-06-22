8dbc6c61d728c7b84a016cafc013fc26589d7823
afradZ <afradzakaria@gmail.com>
2025-06-22 14:50:00 -0700

    initial

---
+++ utils/redis.js
@@ -0,0 +1,46 @@
+import redis from 'redis';
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient({
+      host: process.env.REDIS_HOST || 'localhost',
+      port: process.env.REDIS_PORT || 6379,
+    });
+
+    this.client.on('error', (err) => console.error('Redis error:', err));
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (err, reply) => {
+        if (err) return reject(err);
+        resolve(reply);
+      });
+    });
+  }
+
+  async set(key, value, duration) {
+    return new Promise((resolve, reject) => {
+      this.client.setex(key, duration, value, (err, reply) => {
+        if (err) return reject(err);
+        resolve(reply);
+      });
+    });
+  }
+
+  async del(key) {
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (err, reply) => {
+        if (err) return reject(err);
+        resolve(reply);
+      });
+    });
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
